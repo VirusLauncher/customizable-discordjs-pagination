781aa377efbcb703763fadcdefbd8dd646e10276
Keron Maharaj <kerontm2000@gmail.com>
2022-06-18 21:36:07 -0400

    Support for acknowledged replies
    Added Support for Interactions that were already deferred or replied.
    
    Deferred or replied interactions that were not ephemeral at first cannot be then converted to an ephemeral message

--- src/V14Pagination.js
+++ src/V14Pagination.js
@@ -11,8 +11,8 @@
 const V14Pagination = async (Discord, message, pages, buttons = [], { timeout = 120000, selectMenu = false, selectMenuPlaceholder = 'Select Page', ephemeral = false, resetTimer = true, disableEnd = true }) => {
     if (!pages) throw new Error('Pages are required.');
     if (selectMenu && pages.length > 25) throw new Error('Select menu is only available for upto 25 pages.');
-    if (!selectMenu && (buttons.length <= 1 ||  buttons.length >= 6)) throw new Error(`There must be 2, 3, 4 or 5 buttons provided. You provided ${buttons.length}.`);
-    
+    if (!selectMenu && (buttons.length <= 1 || buttons.length >= 6)) throw new Error(`There must be 2, 3, 4 or 5 buttons provided. You provided ${buttons.length}.`);
+
     // ButtonList
     const buttonList = [];
     let buttonId = [];
@@ -20,16 +20,16 @@ const V14Pagination = async (Discord, message, pages, buttons = [], { timeout = 
     switch (buttons.length) {
         case 2:
             buttonId = ['prevBtn', 'nextBtn'];
-        break;
+            break;
         case 3:
             buttonId = ['prevBtn', 'stopBtn', 'nextBtn'];
-        break;
+            break;
         case 4:
             buttonId = ['firstBtn', 'prevBtn', 'nextBtn', 'lastBtn'];
-        break;
+            break;
         case 5:
             buttonId = ['firstBtn', 'prevBtn', 'stopBtn', 'nextBtn', 'lastBtn'];
-        break;
+            break;
     }
 
     if (buttons.length !== 0) {
@@ -48,7 +48,7 @@ const V14Pagination = async (Discord, message, pages, buttons = [], { timeout = 
         }
         buttonRow = new Discord.ActionRowBuilder().addComponents(buttonList);
     }
-    
+
     // SelectMenu
     const pageOption = [];
     let pageMenu;
@@ -65,7 +65,7 @@ const V14Pagination = async (Discord, message, pages, buttons = [], { timeout = 
             .setCustomId('pageMenu')
             .setOptions(pageOption)
             .setPlaceholder(selectMenuPlaceholder);
-        
+
         pageRow = new Discord.ActionRowBuilder().addComponents(pageMenu);
     }
 
@@ -84,78 +84,89 @@ const V14Pagination = async (Discord, message, pages, buttons = [], { timeout = 
     }
 
     let page = 0;
-    await message.reply({ embeds: [embed(page)], components: components, ephemeral: ephemeral }).then(async(msg) => {
-        const collector = new Discord.InteractionCollector(message.client, {
-            message: message.author ? msg : await message.fetchReply(),
-            time: timeout,
-        });
+    let msg;
 
-        async function editEmbed() {
-            message.author ?
+    if (message.isReplied || message.deferred) {
+        await message.editReply({ embeds: [embed(page)], components: components, ephemeral: ephemeral }).then((m) => {
+            msg = m;
+        });
+    }
+    else {
+        await message.reply({ embeds: [embed(page)], components: components, ephemeral: ephemeral }).then((m) => {
+            msg = m;
+        });
+    }
+
+    const collector = new Discord.InteractionCollector(message.client, {
+        message: message.author ? msg : await message.fetchReply(),
+        time: timeout,
+    });
+
+    async function editEmbed() {
+        message.author ?
             await msg.edit({ embeds: [embed(page)], components: components, fetchReply: true }) :
             await message.editReply({ embeds: [embed(page)], components: components, allowedMentions: { repliedUser: false }, ephemeral: ephemeral });
+    }
+
+    collector.on('collect', async (interaction) => {
+        if (interaction.member.user.id == message.member.id) {
+            if (resetTimer) collector.resetTimer(timeout, timeout);
+            switch (interaction.customId) {
+                case 'firstBtn':
+                    page = 0;
+                    break;
+                case 'lastBtn':
+                    page = pages.length - 1;
+                    break;
+                case 'prevBtn':
+                    page !== 0 ? --page : page = pages.length - 1;
+                    break;
+                case 'nextBtn':
+                    page < pages.length - 1 ? page++ : page = 0;
+                    break;
+                case 'stopBtn':
+                    collector.stop();
+                    break;
+                case 'pageMenu':
+                    page = Number(interaction.values[0]);
+                    break;
+            }
+            await interaction.deferUpdate()
+                .catch(err => {
+
+                });
+            await editEmbed();
         }
-
-        collector.on('collect', async(interaction) => {
-            if (interaction.member.user.id == message.member.id) {
-                if (resetTimer) collector.resetTimer(timeout, timeout);
-                switch(interaction.customId) {
-                    case 'firstBtn':
-                        page = 0;
-                    break;
-                    case 'lastBtn':
-                        page = pages.length - 1;
-                    break;
-                    case 'prevBtn':
-                        page !== 0 ? --page : page = pages.length - 1;
-                    break;
-                    case 'nextBtn':
-                        page < pages.length - 1 ? page++ : page = 0;
-                    break;
-                    case 'stopBtn':
-                        collector.stop();
-                    break;
-                    case 'pageMenu':
-                        page = Number(interaction.values[0]);
-                    break;
-                }
-                await interaction.deferUpdate()
-                    .catch(err => {
-
-                    });
-                await editEmbed();
+        else await interaction.reply({ content: 'This isn\'t your button.', ephemeral: true });
+    });
+    collector.on('end', async () => {
+        let disabledComponents = [];
+        if (disableEnd) {
+            if (selectMenu && buttons.length === 0) {
+                pageMenu.setDisabled(true);
+                const disabledPageMenu = new Discord.ActionRowBuilder().addComponents(pageMenu);
+                disabledComponents = [disabledPageMenu];
             }
-            else await interaction.reply({ content: 'This isn\'t your button.' , ephemeral: true });
-        });
-        collector.on('end', async() => {
-            let disabledComponents = [];
-            if (disableEnd) {
-                if (selectMenu && buttons.length === 0) {
-                    pageMenu.setDisabled(true);
-                    const disabledPageMenu = new Discord.ActionRowBuilder().addComponents(pageMenu);
-                    disabledComponents = [disabledPageMenu];
+            else if (selectMenu && buttons.length !== 0) {
+                pageMenu.setDisabled(true);
+                for (let i = 0; i < buttons.length; i++) {
+                    buttonList[i].setDisabled(true);
                 }
-                else if (selectMenu && buttons.length !== 0) {
-                    pageMenu.setDisabled(true);
-                    for (let i = 0; i < buttons.length; i++) {
-                        buttonList[i].setDisabled(true);
-                    }
-                    const disabledPageMenu = new Discord.ActionRowBuilder().addComponents(pageMenu);
-                    const disabledButtonRow = new Discord.ActionRowBuilder().addComponents(buttonList);
-                    disabledComponents = [disabledPageMenu, disabledButtonRow];
-                }
-                else if (!selectMenu && buttons.length !== 0) {
-                    for (let i = 0; i < buttons.length; i++) {
-                        buttonList[i].setDisabled(true);
-                    }
-                    const disabledButtonRow = new Discord.ActionRowBuilder().addComponents(buttonList);
-                    disabledComponents = [disabledButtonRow];
-                };
+                const disabledPageMenu = new Discord.ActionRowBuilder().addComponents(pageMenu);
+                const disabledButtonRow = new Discord.ActionRowBuilder().addComponents(buttonList);
+                disabledComponents = [disabledPageMenu, disabledButtonRow];
             }
-            message.author ?
+            else if (!selectMenu && buttons.length !== 0) {
+                for (let i = 0; i < buttons.length; i++) {
+                    buttonList[i].setDisabled(true);
+                }
+                const disabledButtonRow = new Discord.ActionRowBuilder().addComponents(buttonList);
+                disabledComponents = [disabledButtonRow];
+            };
+        }
+        message.author ?
             await msg.edit({ embeds: [pages[page]], components: disabledComponents }) :
             await message.editReply({ embeds: [pages[page]], components: disabledComponents, ephemeral: ephemeral });
-        });
     });
 };
 
