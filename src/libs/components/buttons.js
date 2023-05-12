d1218707f3a1220aad287659b7ea447fa37d56fe
Kris Dookharan <59482589+VirusLauncher@users.noreply.github.com>
2023-05-12 15:59:52 +0000

    Pending changes exported from your codespace

--- src/libs/components/buttons.js
+++ src/libs/components/buttons.js
@@ -1,8 +1,10 @@
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle, MessageActionRow, MessageButton, version } = require('discord.js');
 
+const compareVersion = require('../../helpers/compareVersion.js')
+
 module.exports = (buttons) => {
     if (buttons.length <= 1) return null;
-    
+
     const buttonList = [];
     let buttonId = [];
     switch (buttons.length) {
@@ -24,19 +26,19 @@ module.exports = (buttons) => {
         if (!buttons[i].emoji && !buttons[i].label) throw new Error(`Emoji or Label is required. Check button array position ${i}`);
 
         let perButton = null;
-        if (version < '14.0.0') {
+        if (compareVersion(version, '14.0.0') === -1) {
             if (buttons[i].style === 'LINK') throw new Error(`Link styles cannot be used in this package.  Check button array position ${i}`);
 
             perButton = new MessageButton()
                 .setStyle(buttons[i].style)
                 .setCustomId(buttonId[i]);
         }
-        else if (version >= '14.0.0') {
+        else if (compareVersion(version, '14.0.0') === 1) {
             if (buttons[i].style === ButtonStyle.Link) throw new Error(`Link styles cannot be used in this package.  Check button array position ${i}`);
 
             perButton = new ButtonBuilder()
-            .setStyle(buttons[i].style)
-            .setCustomId(buttonId[i])
+                .setStyle(buttons[i].style)
+                .setCustomId(buttonId[i])
         }
 
         if (buttons[i].emoji) perButton.setEmoji(buttons[i].emoji);
@@ -44,6 +46,6 @@ module.exports = (buttons) => {
 
         buttonList.push(perButton);
     }
-    if (version < '14.0.0') return new MessageActionRow().addComponents(buttonList);
+    if (compareVersion(version, '14.0.0') === -1) return new MessageActionRow().addComponents(buttonList);
     return new ActionRowBuilder().addComponents(buttonList);
 }
