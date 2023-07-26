0e962d9ab3a4ac6097e82862fae625c5a5bcc5c9
Kris Dookharan <59482589+VirusLauncher@users.noreply.github.com>
2023-07-26 11:49:25 -0400

    Update handler.js

--- src/libs/collector/handler.js
+++ src/libs/collector/handler.js
@@ -1,9 +1,8 @@
 const embed = require('./embed')
 const collector = require('./collector');
 
-let msg;
-
 module.exports = async (message, components, footer, pages, paginationCollector, customComponentsFunction) => {
+    let msg;
     await (message.isReplied || message.deferred ?
         message.editReply({ embeds: [embed(footer, paginationCollector.startingPage - 1, pages)], components: components, ephemeral: paginationCollector.ephemeral }).then((m) => { msg = m }) :
         message.reply({ embeds: [embed(footer, paginationCollector.startingPage - 1, pages)], components: components, ephemeral: paginationCollector.ephemeral }).then((m) => { msg = m })).catch();
